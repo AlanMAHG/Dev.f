5a2b1c5ab34dbc5d983a066eacfa3e2dd9b60a3b
alanmahg <moisesalanhdz.granados@outlook.com>
2022-01-24 21:29:10 -0600

    Intro a JS

---
+++ KataJS/01-IntroJS/js/index.js
@@ -0,0 +1,7 @@
+// salida de daros
+console.log("Hola mundo desde un archivo js"); //consola
+console.log(4+4);//consola
+document.write("hey world desde unarchivo js"); // en pantalla
+document.write(4>3);// en pantalla
+alert("Hola mundo desde un archivo js");
+alert('3'===3);
